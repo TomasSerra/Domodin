784e392fab738f73b74a8cb7d4a65c7e811cb847
Tomas Serra <tserra@MacBook-Air-de-Tomas.local>
2020-09-21 16:18:44 -0300

    Firebase incluido

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
   selector: 'app-tab1',
@@ -7,18 +8,80 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  luz_estado: boolean = false;
-  aire_estado: boolean = false;
+  luz_estado: boolean;
+  aire_estado: boolean;
+  dispositivos: any [];
   
-  constructor() {}
+  constructor(public afDB: AngularFireDatabase,) {
+   this.getDatos();
+  }
 
   luz()
   {
+    
     this.luz_estado = !this.luz_estado
+
+    if(this.dispositivos[0] == null)
+    {
+      this.afDB.list('/ESP32/').push({
+      luz: this.luz_estado,
+      aire: this.aire_estado
+      });
+    }
+    else
+    {
+      this.afDB.database.ref("/ESP32/" + this.dispositivos[0].key + "/luz").set(this.luz_estado);
+    }
+
+    this.dispositivos[0].luz = this.luz_estado;
+    
   }
 
   aire()
   {
-    this.aire_estado = !this.aire_estado
+    
+    this.aire_estado = !this.aire_estado;
+
+    if(this.dispositivos[0] == null)
+    {
+      this.afDB.list('/ESP32/').push({
+      luz: this.luz_estado,
+      aire: this.aire_estado
+      });
+    }
+    else
+    {
+      this.afDB.database.ref("/ESP32/" + this.dispositivos[0].key + "/aire").set(this.aire_estado);
+    }
+
+    this.dispositivos[0].aire = this.aire_estado;
   }
+
+  getDatos()
+  {
+    this.afDB.list('/ESP32/').snapshotChanges(['child_added', 'child_removed']).subscribe(actions => {
+      this.dispositivos = [];
+      actions.forEach(action => {
+        this.dispositivos.push({
+          key: action.key,
+          luz: action.payload.exportVal().luz, 
+          aire: action.payload.exportVal().aire
+        });
+      });
+    });
+  }
+
+  recargar()
+  {
+    this.getDatos();
+    this.aire_estado = this.dispositivos[0].aire;
+    this.luz_estado = this.dispositivos[0].luz;
+  }
+
+
+
+
+
+
+  
 }
